365d1e23d97ad5d94fc6c41f2466d73d27cffeb8
Martin Gonzalez <tinchogon34@gmail.com>
2012-10-05 16:19:11 -0300

    Command line fixes

--- discover.rb
+++ discover.rb
@@ -1,27 +1,35 @@
 require 'snmp'
 
+gateway, mask, community, options = *ARGV
+
+case options
+when '-h' then 
+  puts "Sintaxis => discover.rb [gateway] [mask] [community] [options]"
+  exit
+end
+
 # get gateway IP
-gateway = `ip route show`.match(/default.*/)[0].match(
+gateway ||= `ip route show`.match(/default.*/)[0].match(
   /\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?/
 )[0]
 
 # get mask
-mask = `ip route show`.match(/.*src.*/)[0].match(/\/\d\d/)[0].match(/\d\d/)
+mask ||= `ip route show`.match(/.*src.*/)[0].match(/\/\d\d/)[0].match(/\d\d/)
 
-system("fping -q -g #{gateway}/#{mask} > /dev/null 2>&1")
+system("fping -q -g #{gateway}/#{mask}")# > /dev/null 2>&1")
 
 ifTable_columns = ["ipNetToMediaPhysAddress", "ipNetToMediaNetAddress"]
 
 puts "MAC\t\t\tIP"
 
-SNMP::Manager.open(Host: gateway, Community: 'mostronave') do |manager|
+SNMP::Manager.open(Host: gateway, Community: (community || 'public')) do |manager|
   manager.walk(ifTable_columns) do |row|
     row.each_with_index do |vb, i|
       if i == 0 
         if vb.value.unpack('H2H2H2H2H2H2').join(':') != 'ff:ff:ff:ff:ff:ff' and vb.value.unpack('H2H2H2H2H2H2').join(':') != '00:00:00:00:00:00'
           print "#{vb.value.unpack('H2H2H2H2H2H2').join(':')}\t"
         else
-         break 
+          break 
         end
       else
         print "#{vb.value}\t"
